bf91b9e74ee4258a9e6e5bfb61425aee2f579221
Ra1na-Jetlearner <raina.sharma0413@gmail.com>
2025-11-03 20:01:59 +0000

    LESSON UPDATE

--- lesson 3/main2.py
+++ lesson 3/main2.py
@@ -2,15 +2,18 @@ import pgzrun
 import random
 
 WIDTH=500
-LENTH=500
+HEIGHT=500
 
 satellite = []
 for i in range(8):
     s=Actor("satellite")
-    s.pos = random.randint(0,500),random.randint(0,500)
+    s.pos = random.randint(50,500),random.randint(500,500)
     satellite.append(s)
 def draw():
     screen.blit("galaxy",(0,0))
+    numb=1
     for s in satellite:
         s.draw()
+        screen.draw.text(str(numb),color="white",center=(s.x,s.y-20))
+        numb+=1
 pgzrun.go()
